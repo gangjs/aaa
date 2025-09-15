9fe31a52a30959f2b0413a472c1dac19ee1a53c0
gangjs <133767921+gangjs@users.noreply.github.com>
2025-09-16 03:26:07 +0900

    Create CashShopOperation.BuyCashItem().java

---
+++ CashShopOperation.BuyCashItem().java
@@ -0,0 +1,19 @@
+else if (action == 5) { // Wishlist  1.2.65 OK
+            // 찜목록 여기 소스 켜버리면 꼬여서 에러생김
+            /*
+            MapleQuestStatus point = chr.getQuestRecord(58585);
+            if (point.getCustomData() == null) {
+                point.setCustomData("0");
+            }
+            int point2 = java.lang.Integer.parseInt(point.getCustomData());
+            if (point2 > 9) {
+                c.getPlayer().dropMessage(1, "캐시샵 재접속 이후 재시도 해주십시오.");
+                doCSPackets(c);
+                return;
+            } else {
+                point.setCustomData((point2 + 1) + "");
+            }
+            */
+
+  ...
+}
